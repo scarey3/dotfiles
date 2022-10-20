fbe8b8b8910c40397d985ddbcdf0c52c6969485d
Sam Carey <sam@scarey.me>
2022-10-20 01:27:50 -0400

    Add krew to path and k8s contexts

--- .oh-my-zsh/custom/custom.zsh
+++ .oh-my-zsh/custom/custom.zsh
@@ -1,4 +1,4 @@
-export PATH="$PATH:$HOME/.rd/bin:$HOME/.pub-cache/bin:$(go env GOPATH)/bin"
+export PATH="$PATH:$HOME/.krew/bin:$HOME/.rd/bin:$HOME/.pub-cache/bin:$(go env GOPATH)/bin"
 
 export CHROME_EXECUTABLE="/usr/bin/chromium"
 export LIBVIRT_DEFAULT_URI="qemu:///system"
