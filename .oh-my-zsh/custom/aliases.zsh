914256e09daecded65951701ffb3b4a82e28f8a0
Sam Carey <sam@scarey.me>
2023-06-09 23:20:12 -0400

    Disable kitty specific aliases when on Windows

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -2,4 +2,8 @@ alias vim="nvim"
 alias vimdiff="nvim -d"
 alias ll="ls -l --color=auto"
 alias snip="maim -s | xclip -selection clipboard -t image/png"
-alias ssh="kitty +kitten ssh"
+
+# Not using kitty on Windows
+if [[ ! $(grep -i microsoft /proc/version) ]]; then
+    alias ssh="kitty +kitten ssh"
+fi
