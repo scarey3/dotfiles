359a6dc90570bc1385a3846436b3835ace2428d2
Sam Carey <sam@scarey.me>
2023-09-27 01:29:10 -0400

    Only check /proc/version on linux

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -3,7 +3,11 @@ alias vimdiff="nvim -d"
 alias ll="ls -l --color=auto"
 alias snip="maim -s | xclip -selection clipboard -t image/png"
 
-# Not using kitty on Windows
-if [[ ! $(grep -i microsoft /proc/version) ]]; then
-    alias ssh="kitty +kitten ssh"
-fi
+case "$OSTYPE" in
+  linux*)
+    # Not using kitty on WSL
+    if [[ ! $(grep -i microsoft /proc/version) ]]; then
+      alias ssh="kitty +kitten ssh"
+    fi
+  ;;
+esac
