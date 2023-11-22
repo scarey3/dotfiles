8b5b4742d9c71f87735fa675c52695fb85ad2a38
Sam Carey <sam@scarey.me>
2023-11-22 15:55:30 -0500

    Update font and enable oh-my-zsh auto update

--- .oh-my-zsh/custom/env.zsh
+++ .oh-my-zsh/custom/env.zsh
@@ -1,4 +1,6 @@
-export PATH="$PATH:$HOME/.krew/bin:$HOME/.rd/bin:$HOME/.pub-cache/bin:$(go env GOPATH)/bin"
+export DISABLE_UPDATE_PROMPT=true
+
+export PATH="$PATH:$HOME/.krew/bin:$HOME/.rd/bin:$HOME/.pub-cache/bin:$(go env GOPATH)/bin:/opt/flutter/bin"
 export CHROME_EXECUTABLE="/usr/bin/chromium"
 export LIBVIRT_DEFAULT_URI="qemu:///system"
 
